7566abeb80c4f3463bcb5f75f4018aea37cf1a97
claudio1975 / claudio + giancaterino <c.giancaterino@gmail.com>
2026-07-11 15:10:14 +0200

    upload notebooks and data

---
+++ 8_export_github.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+df1=pd.read_csv('./work/StormEvents.csv', low_memory=False)
+df2=pd.read_csv('./work/StormEvents_fe_ep_augmentation_fin.csv',low_memory=False)
+
+chunks1=np.array_split(df1, indices_or_sections=2)
+for i, chunk in enumerate(chunks1):
+    chunk.to_parquet(f'./Storm_Events/data/StormEvents_part_{i+1}.parquet', engine='pyarrow', compression='zstd')
+    
+chunks2=np.array_split(df2, indices_or_sections=5)
+for i, chunk in enumerate(chunks2):
+    chunk.to_parquet(f'./Storm_Events/data/StormEvents_fe_ep_augmentation_fin_part_{i+1}.parquet', engine='pyarrow', compression='zstd')
