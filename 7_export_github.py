fd8d04e33d5a58dd09a0cd9cfe28f50ec49640d3
claudio1975 / claudio + giancaterino <c.giancaterino@gmail.com>
2026-09-12 08:03:08 +0200

    update script

--- 7_export_github.py
+++ 7_export_github.py
@@ -2,11 +2,11 @@ import pandas as pd
 import numpy as np
 
 df1=pd.read_csv('./work/StormEvents.csv', low_memory=False)
-df2=pd.read_csv('./work/StormEvents_fe_ep_augmentation_fin.csv',low_memory=False)
+df2=pd.read_csv('./work/StormEvents_fe_ep_augmentation_fin_update.csv',low_memory=False)
 
 chunks1=np.array_split(df1, indices_or_sections=5)
 for i, chunk in enumerate(chunks1):
-    chunk.to_parquet(f'./Storm_Events_GitHub/data/StormEvents_part_update_{i+1}.parquet', engine='pyarrow', compression='zstd')
+    chunk.to_parquet(f'./Storm_Events_GitHub/data/StormEvents_part_{i+1}.parquet', engine='pyarrow', compression='zstd')
     
 chunks2=np.array_split(df2, indices_or_sections=15)
 for i, chunk in enumerate(chunks2):
